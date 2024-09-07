fd13230345ed7507aaa758fff93b932313acb5f9
poopcity <johnnywbrian@gmail.com>
2024-09-07 16:20:41 -0600

    Update game.js

--- game.js
+++ game.js
@@ -25,6 +25,8 @@ let health = 100;
 let roarSound;
 let jumpSound;
 let backgroundMusic;
+let scoreText;
+let healthText;
 
 function preload() {
   this.load.image('godzilla', 'assets/godzilla.PNG');
@@ -48,6 +50,7 @@ function create() {
   
   this.physics.add.collider(godzilla, mario, () => {
     console.log('Collision detected!');
+    health -= 5;
   });
   
   roarSound = this.sound.add('roar');
@@ -55,42 +58,62 @@ function create() {
   backgroundMusic = this.sound.add('backgroundMusic');
   backgroundMusic.play();
   
+  // Keyboard Controls
   this.input.keyboard.createCursorKeys();
   this.input.keyboard.on('keydown_SPACE', () => {
     console.log('Spacebar pressed!');
+  });
+  
+  // Touch Controls
+  this.input.on('pointerdown', (pointer) => {
+    const { x, y } = pointer;
+    
+    // Example touch control for moving godzilla
+    if (x < this.game.config.width / 2) {
+      godzilla.body.setVelocityX(-200);
+    } else {
+      godzilla.body.setVelocityX(200);
+    }
+    
+    if (y < this.game.config.height / 2) {
+      godzilla.body.setVelocityY(-200);
+    } else {
+      godzilla.body.setVelocityY(200);
+    }
   });
   
   this.time.addEvent({
     delay: 1000,
     callback: () => {
       score++;
+      scoreText.setText('Score: ' + score);
     },
     loop: true,
   });
   
-  this.add.text(10, 10, 'Score: 0', {
+  scoreText = this.add.text(10, 10, 'Score: 0', {
     fontSize: '32px',
     fill: '#fff',
   });
   
-  this.add.text(10, 50, 'Health: 100', {
+  healthText = this.add.text(10, 50, 'Health: 100', {
     fontSize: '32px',
     fill: '#fff',
   });
 }
 
 function update(time, delta) {
-  if (this.input.keyboard.isDown(Phaser.Input.Keyboard.LEFT_KEY)) {
+  if (this.input.keyboard.isDown(Phaser.Input.Keyboard.LEFT)) {
     godzilla.body.setVelocityX(-200);
-  } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.RIGHT_KEY)) {
+  } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.RIGHT)) {
     godzilla.body.setVelocityX(200);
   } else {
     godzilla.body.setVelocityX(0);
   }
   
-  if (this.input.keyboard.isDown(Phaser.Input.Keyboard.UP_KEY)) {
+  if (this.input.keyboard.isDown(Phaser.Input.Keyboard.UP)) {
     godzilla.body.setVelocityY(-200);
-  } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.DOWN_KEY)) {
+  } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.DOWN)) {
     godzilla.body.setVelocityY(200);
   } else {
     godzilla.body.setVelocityY(0);
@@ -98,13 +121,11 @@ function update(time, delta) {
   
   if (Phaser.Math.Distance.Between(godzilla.x, godzilla.y, mario.x, mario.y) < 50) {
     health -= 1;
-    this.add.text(10, 50, 'Health: ' + health, {
-      fontSize: '32px',
-      fill: '#fff',
-    });
+    healthText.setText('Health: ' + health);
   }
   
   if (health <= 0) {
     this.scene.restart();
   }
 }
+
